b5112a6c9c32693a5aeff1c21bc6bb60f67934c2
JPawlikowski <pawlikowskijakub29@gmail.com>
2023-09-09 23:31:23 -0400

    order insert POST

--- getUploadOrder.php
+++ getUploadOrder.php
@@ -1,24 +1,16 @@
-<!DOCTYPE html>
-<html>
-<head>
-<style>
-table {
-  width: 100%;
-  border-collapse: collapse;
-}
-
-table, td, th {
-  border: 1px solid black;
-  padding: 5px;
-}
-
-th {text-align: left;}
-</style>
-</head>
-<body>
-
 <?php
-$q = intval($_GET['q']);
+
+$requestPayload = file_get_contents("php://input");
+
+echo $requestPayload;
+
+$payloadDecode = json_decode($requestPayload);
+
+//Note this needs sanitation for missing or invalid format fields
+$payloadOrderId = $payloadDecode->orderId;
+$payloadOrderType = $payloadDecode->orderType;
+$payloadDestination = $payloadDecode->destination;
+
 
 $myfile = fopen("/var/www/html/digitalOMySQLCreds.txt", "r") or die("Unable to open file!");
 //$wmsadmin_creds = fread($myfile,filesize("/var/www/html/digitalOMySQLCreds.txt"));
@@ -33,13 +25,14 @@ $username = "wmsadmin";
 // Create connection
 $conn = new mysqli($servername, $username, $wms_creds);
 
-// Check connection
+// // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 } 
 echo "<p>Connected successfully<p>";
 
-$insert_sql = "INSERT INTO wmssandbox_digitalO.orders (sys_order_id, order_type, order_status, destination_facility) VALUES (876543,'TEST','Created','1234')";
+$insert_sql = "INSERT INTO wmssandbox_digitalO.orders (sys_order_id, order_type, order_status, destination_facility) VALUES ($payloadOrderId, ' $payloadOrderType', 'Created', $payloadDestination)";
+echo $insert_sql;
 
 if ($conn->query($insert_sql) === TRUE) {
   echo "Record inserted successfully";
@@ -50,5 +43,3 @@ if ($conn->query($insert_sql) === TRUE) {
 
 $conn->close();
 ?>
-</body>
-</html>
